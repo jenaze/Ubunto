b9d3e3fe09c0509bff0feda017dc527c589bad38
jenaze <37847028+jenaze@users.noreply.github.com>
2022-12-18 22:36:54 +0000

    first commit

---
+++ hasan.sh
@@ -0,0 +1,13 @@
+#sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+#sudo passwd root
+
+read -p 'enter Iran server ip address : ' ip_iran
+read -p 'enter Out server ip address : ' ip_exter
+
+sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination $ip_iran
+sudo iptables -t nat -A PREROUTING -p icmp --icmp-type 8 -j DNAT --to-destination $ip_iran
+
+#sudo iptables -A FORWARD -p icmp --icmp-type 8 -j ACCEPT --to-destination $ip_iran
+sudo iptables -t nat -A PREROUTING -j DNAT --to-destination $ip_exter
+sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+sudo echo 1 >> /proc/sys/net/ipv4/ip_forward
