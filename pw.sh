f1754e1d70b5d44ec68746d34c3b3bcfae825181
jenaze <37847028+jenaze@users.noreply.github.com>
2022-12-22 12:41:28 +0330

    Update pw.sh

--- pw.sh
+++ pw.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-echo "root:newpassword" | chpasswd
+echo "root:112233" | chpasswd
 service sshd restart
