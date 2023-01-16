e6897ac6ceb832a7e49fb499399eb4ecafab56ba
jenaze <37847028+jenaze@users.noreply.github.com>
2023-01-16 09:32:06 +0330

    Update and rename port to port.sh

--- port.sh
+++ port.sh
@@ -1,3 +1,4 @@
+# bash <(curl -Ls https://raw.githubusercontent.com/jenaze/Ubunto/master/port.sh)
 REQUIRED_PKG="apache2"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
 #echo Checking for $REQUIRED_PKG: $PKG_OK
