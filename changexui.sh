355f3a1a5c79951be0362664228ac30fe738cc5e
jenaze <37847028+jenaze@users.noreply.github.com>
2023-01-27 19:16:20 +0330

    Update changexui.sh

--- changexui.sh
+++ changexui.sh
@@ -1,4 +1,11 @@
-## bash <(curl -Ls https://raw.githubusercontent.com/jenaze/Ubunto/master/changexui.sh)
+
+
+
+#!/bin/bash
+
+ 
+ ## bash <(curl -Ls https://raw.githubusercontent.com/jenaze/Ubunto/master/changexui.sh)
+ 
 REQUIRED_PKG="sqlite3"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
 #echo Checking for $REQUIRED_PKG: $PKG_OK
@@ -7,8 +14,23 @@ if [ "" = "$PKG_OK" ]; then
   sudo apt-get --yes install $REQUIRED_PKG
 fi
 
-read -p 'old ip : ' old_ip
-read -p 'New Ip : ' new_ip
+
+ if [ "$1" -eq  "0" ]
+   then
+     read -p 'old ip : ' old_ip
+ else
+     old_ip = $1
+ fi
+ 
+  if [ "$2" -eq  "0" ]
+   then
+     read -p 'New Ip : ' new_ip
+ else
+     new_ip = $2
+ fi
+ 
+
+
 
 sudo sqlite3 -line /etc/x-ui/x-ui.db "UPDATE inbounds SET listen = '"$new_ip"'"
 sudo sed -i 's/'$old_ip'/'$new_ip'/' /usr/local/x-ui/bin/config.json
