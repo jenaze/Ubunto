15cf02a294855518988bba776f3030f5cab30f41
jenaze <37847028+jenaze@users.noreply.github.com>
2023-02-27 02:57:50 +0330

    Update hasan.sh

--- hasan.sh
+++ hasan.sh
@@ -7,7 +7,7 @@ read -p 'enter Iran server ip address : ' ip_iran
 read -p 'enter Out server ip address : ' ip_exter
 
 sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination $ip_iran
-sudo iptables -t nat -A PREROUTING -p icmp --icmp-type 8 -j DNAT --to-destination $ip_iran
+#sudo iptables -t nat -A PREROUTING -p icmp --icmp-type 8 -j DNAT --to-destination $ip_iran
 
 #sudo iptables -A FORWARD -p icmp --icmp-type 8 -j ACCEPT --to-destination $ip_iran
 sudo iptables -t nat -A PREROUTING -j DNAT --to-destination $ip_exter
