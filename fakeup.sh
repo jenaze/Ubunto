ccccdc6dc13e35e936d8b72fed3c2f9b1784da52
jenaze <37847028+jenaze@users.noreply.github.com>
2023-03-02 03:13:06 +0330

    Update fakeup.sh

--- fakeup.sh
+++ fakeup.sh
@@ -7,20 +7,21 @@
 ## touch /tmp/fakeup.lock
 ## */2 * * * * /dl/1/fakeup.sh >> /dl/1/my.log 2>&1
 
-addr=217.144.107.50
-username=sid@novinlike.ir
-password=mHm09350912
+#!/bin/bash
 
-##################### File Section ######################
+readonly ADDR=217.144.107.50
+readonly USERNAME=sid@novinlike.ir
+readonly PASSWORD=mHm09350912
+readonly UPLOAD_EX=10  # 1X10
+readonly EXTERNAL_DOWNLOAD_MB=0
+readonly EXTERNAL_UPLOAD_MB=0
+readonly MAXIMUM_RANDOM_SPEED=10
+readonly UPLOAD_SPEED_DEFAULT=3m
+
 name=$RANDOM
 x=3
 y=50
-size=$[ $x + $name % ($y + 1 - $x) ]
-##################### Upload Section ######################
-ExteraDownloadMb = 0
-ExteraUploadMb = 0
-UploadEx = 10 //1X10
-
+size=$(( x + name % (y + 1 - x) ))
 
 if [ -f /tmp/fakeup.lock ]; then
     exit 0
@@ -29,43 +30,36 @@ else
 fi
 
 # Get RX and TX statistics (in bytes) for eth0 interface and save to variables
-RX_bytes=$(ifconfig eth0 | awk '/RX packets/ {print $5}')
-TX_bytes=$(ifconfig eth0 | awk '/TX packets/ {print $5}')
+rx_bytes=$(ifconfig eth0 | awk '/RX packets/ {print $5}')
+tx_bytes=$(ifconfig eth0 | awk '/TX packets/ {print $5}')
 
 # Convert RX and TX statistics from bytes to megabytes (MB)
-RX=$(echo "scale=2; $RX_bytes/1048576" | bc -l)
-TX=$(echo "scale=2; $TX_bytes/1048576" | bc -l)
+rx=$(echo "scale=2; ${rx_bytes:6:10}/1048576 + $EXTERNAL_DOWNLOAD_MB" | bc -l)
+tx=$(echo "scale=2; ${tx_bytes:6:10}/1048576 + $EXTERNAL_UPLOAD_MB" | bc -l)
 
-RX=$(echo "$RX+$ExteraDownloadMb" | bc)
-TX=$(echo "$TX+$ExteraUploadMb" | bc)
+sdl=$(echo "$rx*$UPLOAD_EX" | bc)
 
-SDL=$(echo "$RX*$UploadEx" | bc)
+printf "download=%.2f\n" "$rx"
+printf "upload=%.2f\n" "$tx"
+printf "should_upload=%.2f\n" "$sdl"
 
+if [[ $sdl > $tx ]]; then
+    random_size_upload=true
+    upload_speed=$UPLOAD_SPEED_DEFAULT
 
-echo "download=$RX"
-echo "upload=$TX"
-echo "should_upload=$SDL"
+    if $random_size_upload; then
+        upload_speed="$((2 + name % (MAXIMUM_RANDOM_SPEED + 1 - 2)))m"
+        printf "Set UploadSpeed To %s\n" "$upload_speed"
+    fi
 
-if awk 'BEGIN {exit !('"$SDL"' > '"$TX"')}'
-then
-randomSizeUpload=true
-uploadspeed=3m
-
-MaximumRandomSpeed=10
-if $randomSizeUpload
-then
-uploadspeed=$[ 2 + $name % ($MaximumRandomSpeed + 1 - 2) ]m
-echo "Set UploadSpeed To "$uploadspeed
-fi
-
-truncate -s $size'MB' /dl/1/$name.zip
-echo $name".zip ===> "$size"Mb"
-echo $name".zip uploading..."
-curl --limit-rate $uploadspeed -T /dl/1/$name.zip ftp://$addr --user $username:$password &> /dev/null
-curl -v -u $username:$password ftp://$addr -Q 'DELE '$name'.zip' &> /dev/null
-rm -rf /dl/1/$name.zip
-echo "Done"
-echo ""
+    truncate -s "$size"MB /dl/1/"$name".zip
+    printf "%s.zip ===> %d Mb\n" "$name" "$size"
+    printf "%s.zip uploading...\n" "$name"
+    curl --limit-rate "$upload_speed" -T /dl/1/"$name".zip ftp://"$ADDR" --user "$USERNAME":"$PASSWORD" &> /dev/null
+    curl -v -u $username:$password ftp://$addr -Q 'DELE '$name'.zip' &> /dev/null
+    rm -rf /dl/1/$name.zip
+    echo "Done"
+    echo ""
     exit 0
 fi
 # Once the script has finished running, remove the lock file
