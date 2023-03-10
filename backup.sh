80d2e82416e2ec148392746e9defb37c4c9276a4
jenaze <37847028+jenaze@users.noreply.github.com>
2023-03-10 08:39:15 +0330

    Create

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## bash <(curl -Ls https://raw.githubusercontent.com/jenaze/Ubunto/master/backup.sh)
+
+addr=217.144.107.50
+username=sid@novinlike.ir
+password=mHm09350912
+
+curl -T /root/etc/x-ui/x-ui.db ftp://$addr --user $username:$password &> /dev/null
+curl -T /usr/local/x-ui/bin/config.json ftp://$addr --user $username:$password &> /dev/null
+echo "Done"
+echo ""
