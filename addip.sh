479fc1394a84b880ff4aff523d663e6eb833fa28
jenaze <37847028+jenaze@users.noreply.github.com>
2023-08-04 02:48:16 +0330

    Update addip.sh

--- addip.sh
+++ addip.sh
@@ -1,8 +1,23 @@
 # bash <(curl -Ls https://raw.githubusercontent.com/jenaze/Ubunto/master/addip.sh)
 
 #!/bin/bash
-read -p 'enter IP : ' IP_ADDRESS
-NETWORK_INTERFACE=$(ip addr show | grep '^[0-9]' | grep -v ' lo:' | awk '{print $2}')
-echo "ip addr add $IP_ADDRESS/32 dev $NETWORK_INTERFACE" | sudo tee -a /etc/rc.local
-sudo chmod +x /etc/rc.local
-sudo reboot
+read -p 'enter IP(ex:ip/sub) : ' IP_ADDRESS
+
+# File to modify
+file_path="/etc/netplan/50-cloud-init.yaml"
+
+# Backup the original file
+cp "$file_path" "$file_path.bak"
+
+
+# Check if 'addresses' key already exists in the file
+if grep -q "addresses:" "$file_path"; then
+    # If 'addresses' key exists, append the new IP address to the existing list
+    sed -i "/addresses:/ a\ \ \ \ \ \ \ \ \ \ \ \ - $IP_ADDRESS" "$file_path"
+else
+    # If 'addresses' key does not exist, add the new IP address under 'ens3'
+    sed -i "/set-name: ens3/ a\ \ \ \ \ \ \ \ \ \ \ \ addresses:\n\ \ \ \ \ \ \ \ \ \ \ \ - $IP_ADDRESS" "$file_path"
+fi
+
+# Apply the new network configuration
+sudo netplan apply
